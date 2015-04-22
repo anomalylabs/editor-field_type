841a2558b4fef63e6be9d58113a86128461ad0f3
Ryan Thompson <service@aiwebsystems.com>
2015-04-22 11:48:50 -0500

    K

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,10 +1,38 @@
 $(function () {
 
-    $('.ace-editor').each(function () {
+    // Toggle fullscreen mode.
+    $('.editor-field-type a[data-toggle="fullscreen"]').on('click', function (e) {
+
+        e.preventDefault();
+
+        var group = $(this).closest('.editor-field-type');
+        var icon = $(this).find('i');
+
+        // Fullscreen the field group.
+        group.toggleClass('fullscreen');
+
+        // Resize or restore.
+        if (group.hasClass('fullscreen')) {
+
+            group.find('.ace_editor, .ace_content').css('height', $(window).height()).find('textarea').focus();
+            icon.toggleClass('fa-expand').toggleClass('fa-compress');
+        } else {
+
+            group.find('.ace_editor, .ace_content').css('height', group.find('textarea').first().height());
+            icon.toggleClass('fa-expand').toggleClass('fa-compress');
+        }
+    });
+
+    // Initialize editors.
+    $('.editor-field-type textarea').each(function () {
 
         var lang = $(this).data('mode');
         var theme = $(this).data('theme');
 
-        $(this).ace({lang: lang, theme: theme});
+        var editor = $(this).ace({
+            lang: lang,
+            theme: theme,
+            width: $(this).closest('wrapper')
+        });
     });
 });
