f7e082473d6ef752ad879daf8fbbf4cb60142c0b
Ryan Thompson <service@aiwebsystems.com>
2016-12-10 23:37:58 -0600

    Better

--- src/EditorFieldType.php
+++ src/EditorFieldType.php
@@ -160,48 +160,4 @@ class EditorFieldType extends FieldType
 
         return $this->storage;
     }
-
-    /**
-     * Fired after an entry is saved.
-     *
-     * @param EditorFieldType $fieldType
-     */
-    public function onEntrySaved(EditorFieldType $fieldType)
-    {
-        if (!$fieldType->getLocale()) {
-            $this->dispatch(new PutFile($fieldType));
-        }
-    }
-
-    /**
-     * Fired after an entry translation is saved.
-     *
-     * @param EditorFieldType $fieldType
-     */
-    public function onEntryTranslationSaved(EditorFieldType $fieldType)
-    {
-        $this->dispatch(new PutFile($fieldType));
-    }
-
-    /**
-     * Fired after an entry is deleted.
-     *
-     * @param EditorFieldType $fieldType
-     */
-    public function onEntryDeleted(EditorFieldType $fieldType)
-    {
-        if (!$fieldType->getLocale()) {
-            $this->dispatch(new DeleteDirectory($fieldType));
-        }
-    }
-
-    /**
-     * Fired after an entry translation is deleted.
-     *
-     * @param EditorFieldType $fieldType
-     */
-    public function onEntryTranslationDeleted(EditorFieldType $fieldType)
-    {
-        $this->dispatch(new DeleteDirectory($fieldType));
-    }
 }
--- src/EditorFieldTypeServiceProvider.php
+++ src/EditorFieldTypeServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace Anomaly\EditorFieldType;
 
 use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
+use Anomaly\Streams\Platform\Application\Application;
 use Illuminate\Filesystem\Filesystem;
 
 /**
@@ -15,18 +16,22 @@ class EditorFieldTypeServiceProvider extends AddonServiceProvider
 
     /**
      * Register the service provider.
+     *
+     * @param EditorFieldType $fieldType
      */
-    public function register()
+    public function register(EditorFieldType $fieldType)
     {
-        $target = $this->app->make('Anomaly\Streams\Platform\Application\Application')->getAssetsPath(
-            'editor-field_type'
-        );
+
+        $fieldType->on('entry_saved', EditorFieldTypeCallbacks::class . '@onEntrySaved');
+        $fieldType->on('entry_deleted', EditorFieldTypeCallbacks::class . '@onEntryDeleted');
+        $fieldType->on('entry_translation_saved', EditorFieldTypeCallbacks::class . '@onEntryTranslationSaved');
+        $fieldType->on('entry_translation_deleted', EditorFieldTypeCallbacks::class . '@onEntryTranslationDeleted');
 
         /*
          * If the Ace assets don't exist then
          * copy them all over there.
          */
-        if (!is_dir($target)) {
+        if (!is_dir($target = $this->app->make(Application::class)->getAssetsPath('editor-field_type'))) {
 
             /* @var Filesystem $files */
             $files = $this->app->make('files');
