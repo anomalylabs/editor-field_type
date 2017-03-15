3154eae37e1fa0cb0ad6624fd0e1f5d737f5ee2e
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-15 06:29:55 +0300

    Update input.js

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,45 +1,61 @@
 $(document).on('ajaxComplete ready', function () {
-
     // Initialize editors.
-    $('textarea[data-provides="anomaly.field_type.editor"]').each(function () {
+    $('textarea[data-provides="anomaly.field_type.editor"]')
+        .each(function () {
+            var $this = $(this);
+            var $wrapper = $this.closest('.editor-field_type ');
+            var wordWrap = $this.data('word-wrap');
 
-        var lang = $(this).data('mode');
-        var theme = $(this).data('theme');
-        var wrapper = $(this).closest('.editor-field_type ');
+            var $editor = $this.ace({
+                lang: $this.data('mode'),
+                theme: $this.data('theme'),
+                width: $this.closest('editor').width(),
+            });
 
-        var editor = $(this).ace({
-            lang: lang,
-            theme: theme,
-            width: $(this).closest('editor')
-        });
-
-        if ($(this).data('word-wrap') == 'yes') {
-            editor.data('ace').editor.ace.getSession().setUseWrapMode(true);
-        }
-
-        // Toggle fullscreen mode.
-        wrapper.find('[data-toggle="fullscreen"]').on('click', function (e) {
-
-            e.preventDefault();
-
-            var group = $(this).closest('.editor-field_type');
-            var icon = $(this).find('i');
-
-            // Fullscreen the field group.
-            group.toggleClass('fullscreen');
-
-            // Resize or restore.
-            if (group.hasClass('fullscreen')) {
-
-                group.find('.ace_editor, .ace_content').css('height', $(window).height()).find('textarea').focus();
-                icon.toggleClass('fa-expand').toggleClass('fa-compress');
-                window.dispatchEvent(new Event('resize'));
-            } else {
-
-                group.find('.ace_editor, .ace_content').css('height', group.find('textarea').first().height());
-                icon.toggleClass('fa-expand').toggleClass('fa-compress');
-                window.dispatchEvent(new Event('resize'));
+            if (wordWrap === 'yes' || wordWrap === '1') {
+                $editor.data('ace')
+                    .editor.ace.getSession()
+                    .setUseWrapMode(true);
             }
+
+            // Toggle fullscreen mode.
+            $wrapper.find('[data-toggle="fullscreen"]')
+                .on('click', function (e) {
+                    e.preventDefault();
+
+                    var $this = $(this);
+                    var $group = $this.closest('.editor-field_type');
+                    var $icon = $this.find('i');
+
+                    // Fullscreen the field group.
+                    $group.toggleClass('fullscreen');
+
+                    // Resize or restore.
+                    if ($group.hasClass('fullscreen')) {
+                        $group
+                            .find('.ace_editor, .ace_content')
+                            .css('height', $(window).height())
+                            .find('textarea').focus();
+
+                        $icon
+                            .toggleClass('fa-expand')
+                            .toggleClass('fa-compress');
+
+                        window.dispatchEvent(new Event('resize'));
+                    } else {
+                        $group
+                            .find('.ace_editor, .ace_content')
+                            .css(
+                                'height',
+                                $group.find('textarea').first().height()
+                            );
+
+                        $icon
+                            .toggleClass('fa-expand')
+                            .toggleClass('fa-compress');
+
+                        window.dispatchEvent(new Event('resize'));
+                    }
+                });
         });
-    });
 });
