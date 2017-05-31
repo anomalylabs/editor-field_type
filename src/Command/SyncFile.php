e759b87f1556e8313be19ba3a6caca8ea6741e9a
Brennon Loveless <brennon.loveless@gmail.com>
2017-05-31 17:35:11 -0600

    Remove time modified restrition when in production. This makes it so that you don't have to save twice for changes to show up in the admin.

--- src/Command/SyncFile.php
+++ src/Command/SyncFile.php
@@ -73,11 +73,10 @@ class SyncFile
         }
 
         /**
-         * If the file is newer and we're NOT debugging
-         * then update with the file with the database.
+         * If we're NOT debugging and we got this far then we know that the content
+         * has been updated, so write the content to the file.
          */
-        if (filemtime($path) > $entry->lastModified()->timestamp && !$config->get('app.debug')) {
-
+        if (!$config->get('app.debug')) {
             $this->dispatch(new PutFile($this->fieldType));
 
             $content = array_get($entry->getAttributes(), $this->fieldType->getField());
@@ -88,9 +87,7 @@ class SyncFile
          * since that is what we use anyways.
          */
         if (filemtime($path) < $entry->lastModified()->timestamp) {
-
             $this->dispatch(new PutFile($this->fieldType));
-
             $content = array_get($entry->getAttributes(), $this->fieldType->getField());
         }
 
