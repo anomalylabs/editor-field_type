5904653ae4023a92a003bebd0a674f3b1a3f943b
Simon <simon@evoluted.net>
2018-11-20 16:53:46 +0000

    Fix editor init when multiple instances are loaded through ajax

--- resources/js/input.js
+++ resources/js/input.js
@@ -21,8 +21,9 @@ CodeMirror.defineMode("twig_html", function (config) {
      */
     setTimeout(function () {
 
+        // Find uninitialised editors (i.e. not existing ones if loading through ajax)
         let editors = document.querySelectorAll(
-            'textarea[data-provides="anomaly.field_type.editor"]'
+            'textarea[data-provides="anomaly.field_type.editor"]:not([data-editor-init])'
         );
 
         editors.forEach(function (textarea) {
@@ -86,8 +87,9 @@ CodeMirror.defineMode("twig_html", function (config) {
 
             emmetCodeMirror(editor);
 
-            let cm = document.querySelector('.CodeMirror');
-            let cmScroll = document.querySelector('.CodeMirror-scroll');
+            // The CodeMirror div is created immediately after the textarea
+            let cm = textarea.parentElement.querySelector('.CodeMirror');
+            let cmScroll = cm.querySelector('.CodeMirror-scroll');
 
             cm.style.height = 'auto';
             cm.style.minHeight = height;
@@ -108,6 +110,9 @@ CodeMirror.defineMode("twig_html", function (config) {
             $('a[data-toggle="tab"]').on('shown.bs.tab', function () {
                 editor.refresh();
             });
+
+            // Mark this editor as initialised
+            textarea.setAttribute('data-editor-init', true);
         });
     }, 10);
 
